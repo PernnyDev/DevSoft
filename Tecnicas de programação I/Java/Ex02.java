09baa296eada032362af7fee368f45397901cb2d
Vinicius Nascimento Ribeiro <vinicius.ribeiro44@fatec.sp.gov.br>
2023-08-08 23:35:20 -0300

    Ativação do java no vs code

--- Tecnicas de programação I/Java/Ex02.java
+++ Tecnicas de programação I/Java/Ex02.java
@@ -1,24 +1,18 @@
 import java.util.Scanner;
 
-public class Main
-{
-  public static void main (String[]args)
-  {
+public class Ex02 {
+  public static void main(String[] args) {
 
     int salario;
-    System.out.println ("Informe seu salário:");
-    Scanner entrada = new Scanner (System.in);
+    System.out.println("Informe seu salário:");
+    Scanner entrada = new Scanner(System.in);
     salario = entrada.nextInt();
-    
 
     if (salario > 10000)
-      {
-	System.out.println ("Você é muito rico!");
+      System.out.println("Você é muito rico!");
 
-      }
     else
-      {
-      System.out.println ("Trabalhe mais zé mané!!!");}
+      System.out.println("Trabalhe mais zé mané!!!");
 
   }
 }
